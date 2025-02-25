8f3289b127e2cfccd248c645ab294e048173fcd2
IsulaRox <160596607+IsulaRox@users.noreply.github.com>
2025-02-25 13:35:30 +0530

    edit import

--- src/Isula/meeting.js
+++ src/Isula/meeting.js
@@ -1,4 +1,4 @@
-    import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import Avatar from "./Avatar";
 
