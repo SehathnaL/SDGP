f31cff031c6525e966d2be4006fb96cd63f5fdbb
Ayath225 <ayathaluthge@gmail.com>
2025-03-02 08:23:18 +0530

    Extract the the text from the PDF

--- ayath/cv_analyzer.py
+++ ayath/cv_analyzer.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI, File, UploadFile
 import os
 import shutil
+import pdfplumber
 
 app = FastAPI()
 
 UPLOAD_DIR="uploaded_pdfs"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+def extract_text_from_pdf(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        text=text = "\n".join([page.extract_text() for page in pdf.pages if page.extract_text()])
+    return text
 
 
 @app.post("/upload_pdf")
@@ -13,3 +19,6 @@ async def upload_pdf(file: UploadFile = File(...)):
     file_path = os.path.join(UPLOAD_DIR, file.filename)
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+
+    extracted_text=extract_text_from_pdf(file_path)
+    print(extracted_text)
