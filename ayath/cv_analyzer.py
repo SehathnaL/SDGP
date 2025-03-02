e41fdc4e08cfffd2a67c762f6afe765cc6f0c599
Ayath225 <ayathaluthge@gmail.com>
2025-03-02 07:33:57 +0530

    Get the PDF from the post request

--- ayath/cv_analyzer.py
+++ ayath/cv_analyzer.py
@@ -1,3 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import os
+import shutil
 
 app = FastAPI()
+
+UPLOAD_DIR="uploaded_pdfs"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+
+@app.post("/upload_pdf")
+async def upload_pdf(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
