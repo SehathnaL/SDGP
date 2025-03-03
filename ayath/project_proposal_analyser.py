ebc4d9441da41bee561aa083894874df6ef8ac0c
Ayath225 <ayathaluthge@gmail.com>
2025-03-03 20:24:45 +0530

    print all the extracted variables

--- ayath/project_proposal_analyser.py
+++ ayath/project_proposal_analyser.py
@@ -169,6 +169,13 @@ async def upload_pdf(file: UploadFile = File(...)):
     project_object = get_project_objective_from_text(extracted_text)
 
 
+    print("01. Extracted Name:", title)
+    print("02. Project objective:",project_object)
+    print("03.  project_proposal_solution:",project_proposal_solution)
+    print("04.  technologies_tools:", technologies_tools)
+    print("05. Target Audience:", target_audience)
+
+
 
 
 
