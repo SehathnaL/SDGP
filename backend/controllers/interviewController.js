139aa11f261e847cc4ee7c670f3706e55cf5a7fa
amika-d <amika.g.d@gmail.com>
2025-03-21 13:34:57 +0530

    added database controller file

---
+++ backend/controllers/interviewController.js
@@ -0,0 +1,31 @@
+const Interview = require('../models/interview');
+
+
+
+const saveInterview = async (aiPrompt, UserResponse, aiFeedback) => {
+
+    try{
+        let interview = await Interview.findOne(); 
+        
+        if(!interview){
+            interview = new Interview({
+                
+                aiPrompt: [aiPrompt],   // Convert to array
+                UserResponses: [UserResponse],  // Convert to array
+                aiFeedbacks: [aiFeedback] 
+            });
+            
+        }else{
+            interview.aiPrompt.push(aiPrompt);
+            interview.UserResponses.push(UserResponse);
+            interview.aiFeedbacks.push(aiFeedback);
+            
+        }
+        await interview.save();
+        console.log("Interview saved");
+    } catch (error) {
+        console.error("Error saving feedback to DB:", error);
+      }
+    };
+    
+    module.exports = { saveInterview };
